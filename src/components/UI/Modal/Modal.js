8c9fd45b6c7df035e04bab93e403b526f3064b6a
Ananda Muhammad Muthaqin <anandamuhammadmtq@gmail.com>
2020-04-15 11:38:58 +0800

    create the order summary modal

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import css from './Modal.module.css';
+
+const modal = (props) => (
+    <div className={css.Modal}>
+        {props.children}
+    </div>
+);
+
+export default modal;
