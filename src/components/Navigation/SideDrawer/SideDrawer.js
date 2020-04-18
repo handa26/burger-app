77b1a22185b7a1f76cff3d290bf84c9834eba698
Ananda Muhammad Muthaqin <anandamuhammadmtq@gmail.com>
2020-04-18 14:07:43 +0800

    reusing the backdrop and adding functionality into sidedrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,10 +6,15 @@ import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Auxiliary';
 
 const sideDrawer = (props) => {
+    let attachStyle = [css.SideDrawer, css.Close];
+    if(props.open) {
+        attachStyle = [css.SideDrawer, css.Open];
+    }
+
     return(
         <Aux>
-            <Backdrop show />
-            <div className={css.SideDrawer}>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachStyle.join(' ')}>
                 <Logo height="11%" />
                 <nav>
                     <NavItems />
