ddd280f74fb727a30e117681f541c802a72f7183
Ananda Muhammad Muthaqin <anandamuhammadmtq@gmail.com>
2020-04-18 13:52:13 +0800

    fix styling

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,20 @@ import React from 'react';
 import css from './SideDrawer.module.css';
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavigationItems/NavItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
 
 const sideDrawer = (props) => {
     return(
-        <div className={css.SideDrawer}>
-            <Logo height="11%" />
-            <nav>
-                <NavItems />
-            </nav>
-        </div>
+        <Aux>
+            <Backdrop show />
+            <div className={css.SideDrawer}>
+                <Logo height="11%" />
+                <nav>
+                    <NavItems />
+                </nav>
+            </div>
+        </Aux>
     );
 }
 
